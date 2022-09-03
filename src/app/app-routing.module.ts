947eafd2134f1664dda8e65a87da23622263b026
Alejandro Ortiz <aletechbuild@gmail.com>
2022-09-02 17:41:37 -0700

    Pushing To Github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientManagementComponent } from './client-management/client-management.component';
+import { ClientMeetingManagementComponent } from './client-meeting-management/client-meeting-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'client-management', component: ClientManagementComponent}, // route to client creation
+  {path: 'client-meeting-management', component: ClientMeetingManagementComponent} // route to meeting management
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/client-meeting-management/client-meeting-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-meeting-management',
+  templateUrl: './client-meeting-management.component.html',
+  styleUrls: ['./client-meeting-management.component.css']
+})
+export class ClientMeetingManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
